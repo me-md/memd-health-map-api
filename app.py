57eac179a0b8570b881f935bb72ece0492f34a95
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2020-01-06 14:09:28 -0700

    Add args to dynamicly render map based on condition

--- app.py
+++ app.py
@@ -1,20 +1,15 @@
-from flask import Flask, send_file, make_response
+from flask import Flask, send_file, make_response, request
 from plot import render_map
 import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 
-@app.route('/plots/conditions_map', methods=['GET'])
+@app.route('/conditions_map', methods=['GET'])
 def conditions_map():
-    render_map("stroke")
-    plt.savefig("stroke-map.png")
-    return send_file('stroke-map.png', mimetype='image/gif')
-    # bytes_obj = render_map("stroke")
-    #
-    # return send_file(bytes_obj,
-    #                  attachment_filename='plot.png',
-    #                  mimetype='image/png')
-    #
+    args = request.args
+    render_map(args['condition'])
+    plt.savefig("MeMD_Map.png")
+    return send_file('MeMD_Map.png', mimetype='image/gif')
 
 if __name__ == '__main__':
     app.run(debug=True)
